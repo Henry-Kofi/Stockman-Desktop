06cba4bb7ecd462a6b3aa529153e7b4310e8b2b1
Nii Adotei <61387678+Nii-Adotei@users.noreply.github.com>
2022-02-17 19:51:30 +0000

    Update Expenditure.java

--- src/main/java/com/propelquantum/stockmandesktop/Expenditure.java
+++ src/main/java/com/propelquantum/stockmandesktop/Expenditure.java
@@ -8,6 +8,7 @@ public class Expenditure {
     private final StringProperty description;
     private final DoubleProperty amount;
     private final StringProperty dateOfExpenditure;
+    private final IntegerProperty customerID;
 
     Expenditure() {
         expenditureID = new SimpleIntegerProperty();
@@ -15,14 +16,16 @@ public class Expenditure {
         description = new SimpleStringProperty();
         amount = new SimpleDoubleProperty();
         dateOfExpenditure = new SimpleStringProperty();
+        customerID = new SimpleIntegerProperty();
     }
 
-    Expenditure(int id, String expense, String describe, double howMuch, String date) {
+    Expenditure(int id, String expense, String describe, double howMuch, String date, int customer) {
         expenditureID = new SimpleIntegerProperty(id);
         expenses = new SimpleStringProperty(expense);
         description = new SimpleStringProperty(describe);
         amount = new SimpleDoubleProperty(howMuch);
         dateOfExpenditure = new SimpleStringProperty(date);
+        customerID = new SimpleIntegerProperty(customer);
     }
 
     Expenditure(String expense, String describe, double howMuch, String date) {
@@ -31,6 +34,7 @@ public class Expenditure {
         description = new SimpleStringProperty(describe);
         amount = new SimpleDoubleProperty(howMuch);
         dateOfExpenditure = new SimpleStringProperty(date);
+        customerID = new SimpleIntegerProperty();
     }
 
     public void setExpenditureID(final int id) { expenditureID.set(id); }
@@ -43,6 +47,10 @@ public class Expenditure {
 
     public void setDateOfExpenditure(final String date) { dateOfExpenditure.set(date); }
 
+    public void setCustomerID(final int customer) {
+        customerID.set(customer);
+    }
+
     public int getExpenditureID() { return expenditureID.get(); }
 
     public String getExpenses() { return expenses.get(); }
@@ -53,6 +61,10 @@ public class Expenditure {
 
     public String getDateOfExpenditure() { return dateOfExpenditure.get(); }
 
+    public int getCustomerID() {
+        return customerID.get();
+    }
+
     public IntegerProperty expenditureID() { return expenditureID; }
 
     public StringProperty expenses() { return expenses; }
@@ -62,4 +74,8 @@ public class Expenditure {
     public DoubleProperty amount() { return amount; }
 
     public StringProperty dateOfExpenditure() { return dateOfExpenditure; }
+
+    public IntegerProperty customerID() {
+        return customerID;
+    }
 }
