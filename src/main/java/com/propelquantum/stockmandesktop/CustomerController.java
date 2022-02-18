e6582baaaf2d00228abc2fab6314992a3e5be744
Nii Adotei <61387678+Nii-Adotei@users.noreply.github.com>
2022-02-18 20:39:36 +0000

    Update CustomerController.java

--- src/main/java/com/propelquantum/stockmandesktop/CustomerController.java
+++ src/main/java/com/propelquantum/stockmandesktop/CustomerController.java
@@ -1,24 +1,35 @@
 package com.propelquantum.stockmandesktop;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
-public class CustomerController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CustomerController implements Initializable {
 
     public TextField searchTextField;
-    public TableView tableView;
-    public TableColumn idTableColumn;
-    public TableColumn fullNameTableColumn;
-    public TableColumn companyOrLocationTableColumn;
-    public TableColumn totalAmountPurchasedTableColumn;
-    public TableColumn lastPurchasedDateTableColumn;
-    public TableColumn telephoneTableColumn;
+    public TableView<Customer> tableView;
+    public TableColumn<Customer, Integer> idTableColumn;
+    public TableColumn<Customer, String> fullNameTableColumn;
+    public TableColumn<Customer, String> companyOrLocationTableColumn;
+    public TableColumn<Customer, Number> totalAmountPurchasedTableColumn;
+    public TableColumn<Customer, String> lastPurchasedDateTableColumn;
+    public TableColumn<Customer, String> telephoneTableColumn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 
     public void onAddCustomerButtonClicked(ActionEvent actionEvent) {
     }
 
     public void onSearchButtonClicked(ActionEvent actionEvent) {
     }
+
+
 }
