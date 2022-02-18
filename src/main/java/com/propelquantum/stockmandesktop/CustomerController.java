62259be42cf2138b8f5eb14c98fd826982806b41
Nii Adotei <61387678+Nii-Adotei@users.noreply.github.com>
2022-02-18 19:52:27 +0000

    code update
    - added customer controller
    - id and action listeners in customer scene

---
+++ src/main/java/com/propelquantum/stockmandesktop/CustomerController.java
@@ -0,0 +1,24 @@
+package com.propelquantum.stockmandesktop;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class CustomerController {
+
+    public TextField searchTextField;
+    public TableView tableView;
+    public TableColumn idTableColumn;
+    public TableColumn fullNameTableColumn;
+    public TableColumn companyOrLocationTableColumn;
+    public TableColumn totalAmountPurchasedTableColumn;
+    public TableColumn lastPurchasedDateTableColumn;
+    public TableColumn telephoneTableColumn;
+
+    public void onAddCustomerButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void onSearchButtonClicked(ActionEvent actionEvent) {
+    }
+}
