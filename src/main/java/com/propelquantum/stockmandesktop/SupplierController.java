c363e43ccffda00dc599a622c1544aa0a38c3c1c
Nii Adotei <61387678+Nii-Adotei@users.noreply.github.com>
2022-02-19 20:14:07 +0000

    Create SupplierController.java

---
+++ src/main/java/com/propelquantum/stockmandesktop/SupplierController.java
@@ -0,0 +1,13 @@
+package com.propelquantum.stockmandesktop;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SupplierController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        
+    }
+}
