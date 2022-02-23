5bffa2e15e4a874e1d8fff46d7fea84056ba3ae8
Nii Adotei <61387678+Nii-Adotei@users.noreply.github.com>
2022-02-23 13:02:20 +0000

    code update
    - inventory scene responsive fix
    - added inventory controller

---
+++ src/main/java/com/propelquantum/stockmandesktop/InventoryController.java
@@ -0,0 +1,12 @@
+package com.propelquantum.stockmandesktop;
+
+import javafx.event.ActionEvent;
+
+public class InventoryController {
+
+    public void onAddResupplyButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void onSearchButtonClicked(ActionEvent actionEvent) {
+    }
+}
