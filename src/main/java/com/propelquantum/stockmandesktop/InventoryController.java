f6bcf06ed3b10a46fa9f09083d9bb5358be57de9
Nii Adotei <61387678+Nii-Adotei@users.noreply.github.com>
2022-02-23 19:16:23 +0000

    code update
    - tile population in inventory scene

--- src/main/java/com/propelquantum/stockmandesktop/InventoryController.java
+++ src/main/java/com/propelquantum/stockmandesktop/InventoryController.java
@@ -1,12 +1,41 @@
 package com.propelquantum.stockmandesktop;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.FlowPane;
 
-public class InventoryController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class InventoryController implements Initializable {
+
+    public FlowPane flowPane = new FlowPane();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        flowPane.setHgap(40.0);
+        flowPane.setVgap(40.0);
+
+        if (ProductController.products.size() > Main2Controller.productsOnLaunch.size()) {
+            for (var i = 0; i < ProductController.products.size(); i++) {
+                ProductTile productTile = new ProductTile(ProductController.products.get(i).getProductName());
+
+                flowPane.getChildren().add(productTile.asPane());
+            }
+        } else {
+            for (var i = 0; i < Main2Controller.productsOnLaunch.size(); i++) {
+                ProductTile productTile = new ProductTile(Main2Controller.productsOnLaunch.get(i).getProductName());
+
+                flowPane.getChildren().add(productTile.asPane());
+            }
+        }
+
+    }
 
     public void onAddResupplyButtonClicked(ActionEvent actionEvent) {
     }
 
     public void onSearchButtonClicked(ActionEvent actionEvent) {
     }
+
 }
---
+++ src/main/java/com/propelquantum/stockmandesktop/ProductTile.java
@@ -0,0 +1,54 @@
+package com.propelquantum.stockmandesktop;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.TextAlignment;
+
+public class ProductTile {
+    private final Pane pane;
+
+    ProductTile(String nameOfProduct) {
+        Label productName = new Label(nameOfProduct);
+        JFXButton purchaseButton = new JFXButton("Purchase");
+        JFXButton resupplyButton = new JFXButton("Resupply");
+        VBox vBox = new VBox();
+        Pane pane = new Pane();
+
+        pane.setMinSize(200, 20);
+
+        productName.setMinSize(200, 40);
+        productName.setStyle("-fx-background-color: #FFBE0B");
+        productName.setPadding(new Insets(5, 10, 10, 5));
+        productName.setTextFill(Color.BLACK);
+        productName.setAlignment(Pos.CENTER);
+
+        purchaseButton.setMinSize(200, 40);
+        purchaseButton.setTextFill(Color.WHITE);
+        purchaseButton.setStyle("-fx-background-color: #8338EC");
+
+        resupplyButton.setMinSize(200, 40);
+        resupplyButton.setTextFill(Color.WHITE);
+        resupplyButton.setStyle("-fx-background-color: #FF006E");
+
+        vBox.setMinSize(250, 200);
+        vBox.setPadding(new Insets(30, 40, 40, 40));
+        vBox.setStyle("-fx-background-color: #3A86FF");
+
+
+        vBox.getChildren().addAll(productName, pane, resupplyButton, purchaseButton);
+
+        this.pane = new Pane();
+        this.pane.getChildren().add(vBox);
+    }
+
+    public Pane asPane() {
+        return pane;
+    }
+
+
+}
